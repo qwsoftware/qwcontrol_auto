25cf7150260eacce322b52153ef7e20b4d4ce3c8
Lucas Rocha <lucas.rocha@qwsoftware.com.br>
2025-06-17 16:12:15 -0300

    alterações planos de testes

--- Archives/Planos de Testes Geral.sh
+++ Archives/Planos de Testes Geral.sh
@@ -21,20 +21,20 @@ Testar sempre após atualizações:
 SQLserver:
 Testar sempre após atualizações:
 
-# - Execuções de teste respeitando horarios
-# - Fluxo de JOBS
-# - Dependencias entre projetos
-# - ForceRun
-# - ForceRun de JOB pelo monitoramento
-# - Hold/Release(Individual e Full) 
-# - Pendencias da Daily Hab/Des 
-# - Opção para alterar horário de execução do JOB sem alterar o agendamento padrão
-# - Cadastro de Incidente
-# - Execução dos jobs após compose down no periodo do agendamento
-#     - Verificar se o fluxo roda certo, depois de ja ter rodado o fluxo anteriormente na Daily
-# - Entrada correta na Virada da Daily
+- Execuções de teste respeitando horarios
+- Fluxo de JOBS
+- Dependencias entre projetos
+- ForceRun
+- ForceRun de JOB pelo monitoramento
+- Hold/Release(Individual e Full) 
+- Pendencias da Daily Hab/Des 
+- Opção para alterar horário de execução do JOB sem alterar o agendamento padrão
+- Cadastro de Incidente
+- Execução dos jobs após compose down no periodo do agendamento
+    - Verificar se o fluxo roda certo, depois de ja ter rodado o fluxo anteriormente na Daily
+- Entrada correta na Virada da Daily
     - Verificar se Jobs que não devem entrar na Daily
-# - Entrada correta na Virada da Daily com Servidor Off 
-    # - Verificar Pendencias
-# - E-mail
+- Entrada correta na Virada da Daily com Servidor Off 
+    - Verificar Pendencias
+- E-mail
 
