894e5ccf801f7aad9858082baf18c689efcc67e1
Lucas Rocha <lucas.rocha@qwsoftware.com.br>
2025-06-18 14:37:44 -0300

    Planos de testes

--- Archives/Planos de Testes Geral.sh
+++ Archives/Planos de Testes Geral.sh
@@ -1,22 +1,23 @@
 PostgreSQL:
 Testar sempre após atualizações:
 
-- Execuções de teste respeitando horarios 
-- Fluxo de JOBS 
-- Dependencias entre projetos 
-- ForceRun 
+- Execuções de teste respeitando horarios
+- Fluxo de JOBS
+- Dependencias entre projetos
+- ForceRun
 - ForceRun de JOB pelo monitoramento 
-- Hold/Release(Individual e Full) 
-- Pendencias da Daily Hab/Des 
-- Opção para alterar horário de execução do JOB sem alterar o agendamento padrão 
-- Cadastro de Incidente 
+- Hold/Release(Individual e Full)
+- Pendencias da Daily Hab/Des
+- Opção para alterar horário de execução do JOB sem alterar o agendamento padrão
+- Cadastro de Incidente
 - Execução dos jobs após compose down no periodo do agendamento
     - Verificar se o fluxo roda certo, depois de ja ter rodado o fluxo anteriormente na Daily
-- Entrada correta na Virada da Daily 
+- Entrada correta na Virada da Daily
     - Verificar se Jobs que não devem entrar na Daily
-- Entrada correta na Virada da Daily com Servidor Off 
+- Entrada correta na Virada da Daily com Servidor Off
     - Verificar Pendencias
-- E-mail 
+- E-mail
+- Jobs Ciclicos
 
 SQLserver:
 Testar sempre após atualizações:
@@ -37,4 +38,5 @@ Testar sempre após atualizações:
 - Entrada correta na Virada da Daily com Servidor Off 
     - Verificar Pendencias
 - E-mail
+- Jobs Ciclicos
 
