b8c5fa52cc7bab42cce2439acf5d260ac0e4b5bf
Lucas Rocha <lucas.rocha@qwsoftware.com.br>
2025-07-22 10:52:48 -0300

    plano de teste

--- Archives/Planos de Testes Geral.sh
+++ Archives/Planos de Testes Geral.sh
@@ -26,14 +26,14 @@ SQLserver:
 Testar sempre após atualizações:
 
 - Execuções de teste respeitando horarios
-# - Fluxo de JOBS
+- Fluxo de JOBS
 - Dependencias entre projetos
-# - ForceRun
+- ForceRun
 - ForceRun de JOB pelo monitoramento
-- Hold/Release(Individual e Full) 
-# - Pendencias da Daily Hab/Des 
-# - Opção para alterar horário de execução do JOB sem alterar o agendamento padrão
-# - Cadastro de Incidente
+- Hold/Release(Individual e Full)
+- Pendencias da Daily Hab/Des 
+- Opção para alterar horário de execução do JOB sem alterar o agendamento padrão
+- Cadastro de Incidente
 - Execução dos jobs após servidor off no periodo do agendamento
     - Verificar se o fluxo roda certo, depois de ja ter rodado o fluxo anteriormente na Daily
 - Entrada correta na Virada da Daily
@@ -41,8 +41,9 @@ Testar sempre após atualizações:
     - Verificar se Jobs que não devem entrar na Daily
 - Entrada correta na Virada da Daily com Servidor Off 
     - Verificar Pendencias
-# - E-mail
-# - Jobs Ciclicos
-    # - Reagendamento correto em caso de exceder horário
-    # - Flag de parar ou contuniar o agendamento
+- E-mail
+- Jobs Ciclicos
+    - Reagendamento correto em caso de exceder horário
+    - Flag de parar ou contuniar o agendamento
+    - Comportamento ao derrubar o servidor com job ciclico rodando
 
