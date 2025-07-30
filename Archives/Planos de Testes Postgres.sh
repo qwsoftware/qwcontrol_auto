aa56119c1d3face85d26bda59056cfc1a3317e34
Lucas Rocha <lucas.rocha@qwsoftware.com.br>
2025-07-30 10:08:42 -0300

    Alteração planos de testes

---
+++ Archives/Planos de Testes Postgres.sh
@@ -0,0 +1,26 @@
+PostgreSQL:
+Testar sempre após atualizações:
+
+- Execuções de teste respeitando horarios
+- Fluxo de JOBS
+- Dependencias entre projetos
+- ForceRun
+- ForceRun de JOB pelo monitoramento #Não está respeitando o agendamento)
+- Hold/Release(Individual e Full)
+- Pendencias da Daily Hab/Des
+- Opção para alterar horário de execução do JOB sem alterar o agendamento padrão
+- Cadastro de Incidente
+- Execução dos jobs após compose down no periodo do agendamento
+    - Verificar se o fluxo roda certo, depois de ja ter rodado o fluxo anteriormente na Daily
+- Entrada correta na Virada da Daily
+    - Verificar se Jobs que não devem entrar na Daily
+    - Jobs rodando na virada da Daily
+- Entrada correta na Virada da Daily com Servidor Off
+    - Verificar Pendencias
+- E-mail
+- Jobs Ciclicos
+    - Reagendamento correto em caso de exceder horário
+    - Flag de parar ou contuniar o agendamento
+    - Comportamento ao derrubar o servidor com job ciclico rodando
+
+Novos Testes:
